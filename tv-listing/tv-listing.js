07fc5ec2a626ae678eb1d91bfbe5dcc0f4dc523e
Ken Tabor <katworks@gmail.com>
2015-07-19 16:23:54 -0500

    tv-listing component select binds to an attribute that's used on service request

--- tv-listing/tv-listing.js
+++ tv-listing/tv-listing.js
@@ -37,11 +37,10 @@ CoolButton = Polymer({
 
   // event handlers
   onClicked: function() {
-    console.log(this.$.genres.value);
-    this.listingLoad(this.$.genres.value);
+    this.listingLoad();
   },
 
-  listingLoad: function(genre) {
+  listingLoad: function() {
     var GENRE_URLS = {
       comedy: 'tv-listing/mock/comedy_standup_mock.json',
       crime: 'tv-listing/mock/drama_crime_mock.json',
@@ -49,30 +48,26 @@ CoolButton = Polymer({
       music: 'tv-listing/mock/music_classicalpoprock_mock.json',
       football: 'tv-listing/mock/sport_football_mock.json'
     };
-    var url = GENRE_URLS[genre];
+
+    var url = GENRE_URLS[this.genre];
     var request = new XMLHttpRequest();
-    var listing = {};
     var _this = this;
 
     request.open('GET', url, true);
     request.onload = function() {
       if (request.status >= 200 && request.status < 400) {
-        // Success!
         var response = JSON.parse(request.responseText);
+
         _this.listingResults = response.broadcasts;
         _this.listingShow();
-
-        console.log('server responded success', _this.listingResults);
       } else {
-        console.log('server responded an error');
+        console.log('server responded an error', request);
       }
     };
     request.onerror = function() {
       console.log('connection error');
     };
     request.send();
-
-    return listing;
   },
 
   attributeChanged: function(name, type) {
