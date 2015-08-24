6052bc274363be5afda20d7c60a4a563bbfc7c4d
Ken Tabor <katworks@gmail.com>
2015-08-23 20:58:16 -0500

    hide cool-buttons to start and reveal first and remainder on button clicks

--- main_buttons.js
+++ main_buttons.js
@@ -6,15 +6,21 @@ function ready(fn) {
 
 
 ready(function onReady() {
-  var btn = document.querySelector('#btn-accept');
   var btnOrdinary = document.getElementById('btn-ordinary');
+  var btnAccept = document.querySelector('#btn-accept');
 
+  var spanHold1 = document.querySelector('#action-bar-1');
+  var spanHold2 = document.querySelector('#action-bar-2');
 
-  btn.addEventListener('clicked', function(e) {
-    alert('button tickled the page [' + JSON.stringify(e.detail) + ']');
-  });
 
   btnOrdinary.addEventListener('click', function(e) {
     alert('much ordinary');
+    spanHold1.style.display = 'inherit';
   });
+
+  btnAccept.addEventListener('clicked', function(e) {
+    alert('button tickled the page [' + JSON.stringify(e.detail) + ']');
+    spanHold2.style.display = 'inherit';
+  });
+
 });
